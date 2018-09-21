6be546552f623e57843d82cd70ded727fd3cf751
chewdev <itsbewald@gmail.com>
2018-09-21 01:26:38 -0700

    Added Array.from method examples

--- arrays.js
+++ arrays.js
@@ -26,3 +26,28 @@ Array.prototype.toBool = function() {
 
 var boolArr = myArr.toBool();
 console.log(boolArr);
+
+var fromArr = Array.from("john"); // ['j', 'o', 'h', 'n']
+// We can pass a map function as second argument, called on every element as in the .map method
+var fromArr2 = Array.from([1, 5, 10], x => x * x); // [1, 25, 100]
+var fromArr3 = Array.from({ 1: "bob", 2: "john", 3: "sally" }); // []
+var fromArr4 = Array.from("first", x => x + "-"); // ['f-', 'i-', 'r-', 's-', 't-']
+
+function Person(lastname) {
+  this.lastname = lastname;
+  return this;
+}
+
+var johnson = new Person("Johnson");
+
+// We can optionally pass a 3rd argument.
+// That argument will be the 'this' for the map function to reference
+var fromArr5 = Array.from(
+  ["bob", "jane", "joe"],
+  function(x) {
+    return x + " " + this.lastname;
+  },
+  johnson
+); // ['bob Johnson', 'jane Johnson', 'joe Johnson']
+
+console.log(fromArr, fromArr2, fromArr3, fromArr4, fromArr5);
