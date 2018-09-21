cc4700aab200d7acafbcef4777b2a64fc3a1a0b9
chewdev <itsbewald@gmail.com>
2018-09-20 23:41:11 -0700

    Initial commit

---
+++ arrays.js
@@ -0,0 +1,28 @@
+// Array.length
+var myArr = [
+  1,
+  2,
+  3,
+  "bob",
+  null,
+  true,
+  { concsistOf: "anything" },
+  ["other", "nested", "array"],
+  false,
+  0
+];
+
+var length = myArr.length; // 8
+console.log("length", length);
+
+// Array.prototype - We can add new methods onto the array prototype
+Array.prototype.toBool = function() {
+  var newArr = [];
+  this.forEach(item => {
+    newArr.push(!!item);
+  });
+  return newArr;
+};
+
+var boolArr = myArr.toBool();
+console.log(boolArr);
