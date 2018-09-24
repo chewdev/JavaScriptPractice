fecc670c023f7abd27fb8c4684cf25d49beb1053
chewdev <itsbewald@gmail.com>
2018-09-23 17:18:04 -0700

    Added functions.js file with notes on some of the deprecated / obsolete properties of functions

---
+++ functions.js
@@ -0,0 +1,13 @@
+// Function.arguments is deprecated and should not be used
+// Now the simple variable arguments should be used within the function
+
+// Function.arity is deprecated / obsolete and should not be used either
+// This property used to return the number of arguments expected by the function
+// .arity has been replaced by Function.prototype.length property
+
+// Function.caller is a non-standard and should not be used in production web apps
+// The expectation is that this method would return the function that invoked the specified function
+
+// Function.displayName is a non-standard and should not be used in production web apps
+// The expectation is that this method should return the display name of the function
+// display name is a property on the function that can be assigned
