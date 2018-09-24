d1947c259134a14ca3a84df6715d7d52d77e86c4
chewdev <itsbewald@gmail.com>
2018-09-23 17:27:33 -0700

    Added Function.length property practice/examples

--- functions.js
+++ functions.js
@@ -11,3 +11,21 @@
 // Function.displayName is a non-standard and should not be used in production web apps
 // The expectation is that this method should return the display name of the function
 // display name is a property on the function that can be assigned
+
+// Function.length
+// This property of functions returns the number of arguments expected by the function
+
+function noArgs() {
+  console.log("none");
+}
+function args2(a, b) {
+  console.log(a, b);
+}
+function restArgs(...rest) {
+  console.log(rest);
+}
+console.log(noArgs.length); // 0
+console.log(args2.length); // 2
+console.log(restArgs.length); // 0, rest parameter is not counted
+
+// Function.length is an ES 1st Edition specification and is available on all browsers
