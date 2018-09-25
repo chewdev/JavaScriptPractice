7b0fd122cc61d4d8a8c2a66cd81297f94ab012a5
chewdev <itsbewald@gmail.com>
2018-09-25 00:14:36 -0700

    Added Object.assign practice/examples

--- objects.js
+++ objects.js
@@ -35,3 +35,42 @@ Child.prototype = Object.create(Parent.prototype); // re-define child prototype 
 Child.prototype.constructor = Child; // return original constructor to Child
 
 // .constructor is an ES 1st Edition specification and is available in all browsers
+
+// Object.assign()
+// This method is used to copy the values of all enumerable own properties from one or more objects into a target object.
+// The altered target object is returned
+
+var objectToAssign = {
+  bob: "1st",
+  joe: "2nd",
+  john: "3rd"
+};
+
+// Object.assign(target, ...sources)
+var assignedObject = Object.assign(
+  { jill: "4th", jack: "5th" },
+  objectToAssign
+);
+console.log(assignedObject);
+console.log(objectToAssign);
+
+// copy object (won't be a pointer to the reference object and can be changed without changing the source object)
+var copyObjectToAssign = Object.assign({}, objectToAssign);
+console.log(copyObjectToAssign); // {bob: '1st', joe: '2nd', john: '3rd'}
+copyObjectToAssign.billy = "4th";
+console.log(copyObjectToAssign); // {bob: '1st', joe: '2nd', john: '3rd', billy: '4th'}
+console.log(objectToAssign); // {bob: '1st', joe: '2nd', john: '3rd'}
+
+var refObjectToAssign = objectToAssign; // points to reference of object, will mutate original objectToAssign if changed
+console.log(refObjectToAssign); // {bob: '1st', joe: '2nd', john: '3rd'}
+refObjectToAssign.will = "4th";
+console.log(refObjectToAssign); // {bob: '1st', joe: '2nd', john: '3rd', will: '4th'}
+console.log(objectToAssign); // {bob: '1st', joe: '2nd', john: '3rd', will: '4th'} this was also mutated
+
+// This does not perform a deep clone of objects
+// It will not return a reference to the top level object, but objects within that object will return a reference
+// If a deep clone that will not mutate any values in the original object is needed, we can use:
+var notMutatingObj = JSON.parse(JSON.stringify(objectToAssign));
+
+// .assign is an ES6 specification and is not currently supported in IE, Android, and Opera Android
+// A polyfill is available on MDN if needed
